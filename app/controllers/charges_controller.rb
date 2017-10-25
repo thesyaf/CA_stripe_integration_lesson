6510575b913e6229a4abe27e5b2396ff1c548a0c
Syaf Djamil <the.syaf@gmail.com>
2017-10-25 14:55:03 +1100

    storing stripe id into user table

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -12,17 +12,31 @@ class ChargesController < ApplicationController
     # Amount in cents
     @amount = 500
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+    if current_user.stripe_id?
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+      charge = Stripe::Charge.create(
+        :customer    => user.stripe_id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+
+    else
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+
+      current_user.stripe_id = customer.id
+      current_user.save
+
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+    end
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
